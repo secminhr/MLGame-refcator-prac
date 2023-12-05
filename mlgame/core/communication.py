61221d4795972ea52894d8678f1e647c6fb26cdc
secminhr <secminhrian@gmail.com>
2023-12-05 11:25:32 +0800

    Use CommunicationHandler in CommunicationSet. There's no reason to separate send/recv end in CommunicationSet

--- mlgame/core/communication.py
+++ mlgame/core/communication.py
@@ -4,132 +4,6 @@ from queue import Queue
 from mlgame.core.env import WS_WAIT_GAME_TIMEOUT
 
 
-class CommunicationSet:
-    """
-    A data class for storing a set of communication objects and
-    providing an interface for accessing these objects
-
-    Communication objects used for receiving objects must provide `recv()` and `poll()`,
-    and them used for sending objects must provide `send()`.
-    For example, the object of `multiprocessing.connection.Connection` is a valid
-    communication object for both sending and receiving.
-
-    @var _recv_end A dictionary storing communication objects which are used to
-         receive objects
-    @var _send_end A dictionary storing communication objects which are used to
-         send objects
-    """
-
-    def __init__(self):
-        self._recv_end = {}
-        self._send_end = {}
-
-    def add_recv_end(self, name: str, comm_obj):
-        """
-        Add a new communication object for receiving objects
-
-        @param name The name for distinguishing the added object. It could be used to
-               distinguish the communication target.
-        @param comm_obj The communication object which has `recv()` and `poll()` functions
-        """
-        if self._recv_end.get(name):
-            raise ValueError("The name '{}' already exists in 'recv_end'".format(name))
-
-        if not hasattr(comm_obj, "recv") or not hasattr(comm_obj, "poll"):
-            raise ValueError("'comm_obj' doesn't have 'recv' or 'poll' function")
-
-        self._recv_end[name] = comm_obj
-
-    def add_send_end(self, name: str, comm_obj):
-        """
-        Add a new communication object for sending objects
-
-        @param name The name for distinguishing the added object. It could be used to
-               distinguish the communication target.
-        @param comm_obj The communication object which has `send()` function
-        """
-        if self._send_end.get(name):
-            raise ValueError("The name '{}' already exists in 'send_end'".format(name))
-
-        if not hasattr(comm_obj, "send"):
-            raise ValueError("'comm_obj' doesn't have 'send' function")
-
-        self._send_end[name] = comm_obj
-
-    def get_recv_end_names(self):
-        """
-        Get the name of all registered communication objects used for receiving
-
-        @return A dictionary view objects containing the key of `_recv_end`
-        """
-        return self._recv_end.keys()
-
-    def get_send_end_names(self):
-        """
-        Get the name of all registered communication objects used for sending
-
-        @return A dictionary view objects containing the key of `_send_end`
-        """
-        return self._send_end.keys()
-
-    def poll(self, name: str):
-        """
-        Check whether the specified communication object has data to read
-
-        @param name The name of the communication object
-        """
-        return self._recv_end[name].poll()
-
-    def recv(self, name: str, to_wait: bool = False):
-        """
-        Receive object from the specified communication object
-
-        @param name The name of the communication object
-        @param to_wait Whether to wait until the object is arrived
-        @return The received object. If `to_wait` is False and nothing available from
-                the specified communication object, return None.
-        """
-        if not to_wait and not self.poll(name):
-            return None
-
-        return self._recv_end[name].recv()
-
-    def recv_all(self, to_wait: bool = False):
-        """
-        Receive objects from all communication object registered for receiving
-
-        If `to_wait` is True, it will wait for the object one by one.
-
-        @param to_wait Whether to wait until the object is arrived
-        @return A dictionary storing received objects. The key is the name of
-                the communication object, the value is the received object.
-                If `to_wait` is False and nothing to receive, the value will be None.
-        """
-        objs = {}
-        for comm_name in self._recv_end.keys():
-            objs[comm_name] = self.recv(comm_name, to_wait)
-
-        return objs
-
-    def send(self, obj, name: str):
-        """
-        Send object via the specified communication object
-
-        @param obj The object to be sent
-        @param name The name of the communication object
-        """
-        self._send_end[name].send(obj)
-
-    def send_all(self, obj):
-        """
-        Send object via all communication objects registered for sending
-
-        @param obj The object to be sent
-        """
-        for comm_obj in self._send_end.values():
-            comm_obj.send(obj)
-
-
 class CommunicationHandler:
     """
     A data class for storing a sending and a receiving communication objects
@@ -172,6 +46,91 @@ class CommunicationHandler:
         self._send_end.send(obj)
 
 
+class CommunicationSet:
+    """
+    A data class for storing a set of communication objects and
+    providing an interface for accessing these objects
+
+    Communication objects used for receiving objects must provide `recv()` and `poll()`,
+    and them used for sending objects must provide `send()`.
+    For example, the object of `multiprocessing.connection.Connection` is a valid
+    communication object for both sending and receiving.
+
+    @var _recv_end A dictionary storing communication objects which are used to
+         receive objects
+    @var _send_end A dictionary storing communication objects which are used to
+         send objects
+    """
+
+    def __init__(self):
+        self._comm_handlers: dict[str, CommunicationHandler] = {}
+
+    def add_comm_handler(self, name: str, comm: CommunicationHandler):
+        if name in self._comm_handlers:
+            raise ValueError("The name '{}' already exists in 'comm_handlers'".format(name))
+
+        self._comm_handlers[name] = comm
+
+    def get_comm_handler_names(self):
+        return self._comm_handlers.keys()
+
+    def poll(self, name: str):
+        """
+        Check whether the specified communication object has data to read
+
+        @param name The name of the communication object
+        """
+        return self._comm_handlers[name].poll()
+
+    def recv(self, name: str, to_wait: bool = False):
+        """
+        Receive object from the specified communication object
+
+        @param name The name of the communication object
+        @param to_wait Whether to wait until the object is arrived
+        @return The received object. If `to_wait` is False and nothing available from
+                the specified communication object, return None.
+        """
+        if not to_wait and not self.poll(name):
+            return None
+
+        return self._comm_handlers[name].recv()
+
+    def recv_all(self, to_wait: bool = False):
+        """
+        Receive objects from all communication object registered for receiving
+
+        If `to_wait` is True, it will wait for the object one by one.
+
+        @param to_wait Whether to wait until the object is arrived
+        @return A dictionary storing received objects. The key is the name of
+                the communication object, the value is the received object.
+                If `to_wait` is False and nothing to receive, the value will be None.
+        """
+        objs = {}
+        for comm_name in self.get_comm_handler_names():
+            objs[comm_name] = self.recv(comm_name, to_wait)
+
+        return objs
+
+    def send(self, obj, name: str):
+        """
+        Send object via the specified communication object
+
+        @param obj The object to be sent
+        @param name The name of the communication object
+        """
+        self._comm_handlers[name].send(obj)
+
+    def send_all(self, obj):
+        """
+        Send object via all communication objects registered for sending
+
+        @param obj The object to be sent
+        """
+        for comm_handlers in self._comm_handlers.values():
+            comm_handlers.send(obj)
+
 class GameCommManager:
     """
     The commnuication manager for the game process
@@ -185,14 +144,16 @@ class GameCommManager:
         """
         Set communication objects for communicating with specified ml process
         """
-        self._comm_to_ml_set.add_recv_end(ml_name, recv_end)
-        self._comm_to_ml_set.add_send_end(ml_name, send_end)
+        comm_handler = CommunicationHandler()
+        comm_handler.set_recv_end(recv_end)
+        comm_handler.set_send_end(send_end)
+        self._comm_to_ml_set.add_comm_handler(ml_name, comm_handler)
 
     def get_ml_names(self):
         """
         Get the name of all registered ml process
         """
-        return self._comm_to_ml_set.get_recv_end_names()
+        return self._comm_to_ml_set.get_comm_handler_names()
 
     def send_to_ml(self, obj, ml_name):
         """
@@ -219,17 +180,16 @@ class GameCommManager:
         """
         Receive objects from all the ml processes
         """
-        obj_dict = {}
-        for ml_name in self.get_ml_names():
-            obj_dict[ml_name] = self.recv_from_ml(ml_name)
-        return obj_dict
+        return self._comm_to_ml_set.recv_all(to_wait=False)
 
     def add_comm_to_others(self, client_name, recv_end, send_end):
         """
         Set communication objects for communicating with specified ml process
         """
-        self._comm_to_others.add_recv_end(client_name, recv_end)
-        self._comm_to_others.add_send_end(client_name, send_end)
+        comm_handler = CommunicationHandler()
+        comm_handler.set_recv_end(recv_end)
+        comm_handler.set_send_end(send_end)
+        self._comm_to_others.add_comm_handler(client_name, comm_handler)
 
     def send_to_others(self, obj):
         """
@@ -242,10 +202,7 @@ class GameCommManager:
         """
         Receive objects from all the ml processes
         """
-        obj_dict = {}
-        for client_name in self._comm_to_others.get_recv_end_names():
-            obj_dict[client_name] = self._comm_to_others.recv(client_name, to_wait=False)
-        return obj_dict
+        return self._comm_to_others.recv_all(to_wait=False)
 
 
 class MLCommManager:
