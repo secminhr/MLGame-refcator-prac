52bd9c687a7ec223181060e0f708c69094867d4b
secminhr <secminhrian@gmail.com>
2023-12-28 17:58:07 +0800

    test ai reset, game end, update

--- mlgame/tests/test_ai_executor.py
+++ mlgame/tests/test_ai_executor.py
@@ -1,4 +1,5 @@
 from unittest.mock import Mock
+from typing import Callable
 
 import pytest
 
@@ -28,6 +29,9 @@ class RecvEnd:
         pass
 
 
+def fixed_ai_loader(ai: AIClient) -> Callable[[str, dict], AIClient]:
+    return lambda ai_name, game_params: ai
+
 class TestAIExecutor:
     @pytest.mark.parametrize(
         'game_status_list',
@@ -37,21 +41,62 @@ class TestAIExecutor:
         ]
     )
     def test_ai_reset(self, game_status_list):
-        # setup communication
+        ai_comm = self._create_mocked_ai_comm(game_status_list)
+        ai_client = self._patch_ai_client_reset()
+        executor = AIClientExecutor('tester', ai_comm, ai_loader=fixed_ai_loader(ai_client))
+        executor.run()
+
+        assert ai_client.reset.call_count == len(game_status_list) - 1
+
+    def _patch_ai_client_reset(self) -> AIClient:
+        # patch ai client
+        ai_client = MLPlay()
+        ai_client.update = Mock(return_value="RESET")
+        ai_client.reset = Mock()
+        return ai_client
+
+    @pytest.mark.parametrize(
+        'game_status_list',
+        [
+            [({'status': 'GAME_ALIVE'}, {})] * 5 + [({'status': 'GAME_OVER'}, {}), None],
+            [({'status': 'GAME_ALIVE'}, {})] * 10 + [({'status': 'GAME_OVER'}, {}), None]
+        ]
+    )
+    def test_ai_game_end(self, game_status_list):
+        ai_comm = self._create_mocked_ai_comm(game_status_list)
+        ai_client = self._patch_ai_client()
+
+        executor = AIClientExecutor('tester', ai_comm, ai_loader=fixed_ai_loader(ai_client))
+        executor.run()
+
+        ai_client.reset.assert_called_once()
+
+    @pytest.mark.parametrize(
+        'game_status_list',
+        [
+            [({'status': 'GAME_ALIVE'}, {})] * 5 + [({'status': 'GAME_OVER'}, {}), None],
+            [({'status': 'GAME_ALIVE'}, {})] * 10 + [({'status': 'GAME_OVER'}, {}), None]
+        ]
+    )
+    def test_ai_update(self, game_status_list):
+        ai_comm = self._create_mocked_ai_comm(game_status_list)
+        ai_client = self._patch_ai_client()
+
+        executor = AIClientExecutor('tester', ai_comm, ai_loader=fixed_ai_loader(ai_client))
+        executor.run()
+
+        assert ai_client.update.call_count == len(game_status_list) - 1
+
+    def _patch_ai_client(self) -> AIClient:
+        # patch ai client
+        ai_client = MLPlay()
+        ai_client.update = Mock()
+        ai_client.reset = Mock()
+        return ai_client
+
+    def _create_mocked_ai_comm(self, game_status_list):
         ai_comm = MLCommManager('tester')
         recv_end = RecvEnd()
         recv_end.recv = Mock(side_effect=game_status_list)
         ai_comm.set_comm_to_game(recv_end, SendEnd())
-
-        executor = AIClientExecutor('tester', ai_comm, ai_loader=self._patch_ai_client_reset)
-        executor.run()
-
-        assert self.ai_client.reset.call_count == len(game_status_list) - 1
-
-    def _patch_ai_client_reset(self, ai_name: str, game_params: dict) -> AIClient:
-        # patch ai client
-        self.ai_client = MLPlay()
-        self.ai_client.update = Mock(return_value="RESET")
-        self.ai_client.reset = Mock()
-        return self.ai_client
-
+        return ai_comm
