eb92d6af5d4b57f1b1a1de6a4a3759bdfc1df092
secminhr <secminhrian@gmail.com>
2023-12-29 13:08:37 +0800

    use mock_calls to fully test about what's called on ai client and what is sent to game

--- mlgame/tests/test_ai_executor.py
+++ mlgame/tests/test_ai_executor.py
@@ -1,3 +1,4 @@
+from contextlib import contextmanager
 from unittest.mock import Mock
 from typing import Callable
 
@@ -32,71 +33,73 @@ class RecvEnd:
 def fixed_ai_loader(ai: AIClient) -> Callable[[str, dict], AIClient]:
     return lambda ai_name, game_params: ai
 
+
+@contextmanager
+def assert_same(mock_calls):
+    expected_calls = Mock()
+    try:
+        yield expected_calls
+    finally:
+        assert mock_calls == expected_calls.mock_calls
+
+
 class TestAIExecutor:
-    @pytest.mark.parametrize(
-        'game_status_list',
-        [
-            [({'status': 'GAME_ALIVE'}, {})] * 5 + [None],
-            [({'status': 'GAME_ALIVE'}, {})] * 10 + [None]
-        ]
-    )
-    def test_ai_reset(self, game_status_list):
-        ai_comm = self._create_mocked_ai_comm(game_status_list)
+    @pytest.mark.parametrize('game_alive_count', [5, 10])
+    def test_ai_reset(self, game_alive_count):
+        game_status_list = [({'status': 'GAME_ALIVE'}, {})] * game_alive_count + [None]
+        ai_comm, send_to_game = self._create_mocked_ai_comm(game_status_list)
         ai_client = self._patch_ai_client_reset()
+
+        func_calls_test = Mock()
+        func_calls_test.ai_client = ai_client
+        func_calls_test.send_to_game = send_to_game.send
+
         executor = AIClientExecutor('tester', ai_comm, ai_loader=fixed_ai_loader(ai_client))
         executor.run()
 
-        assert ai_client.reset.call_count == len(game_status_list) - 1
+        with assert_same(func_calls_test.mock_calls) as expect_calls:
+            for i in range(game_alive_count):
+                expect_calls.send_to_game('READY')
+                expect_calls.ai_client.update({'status': 'GAME_ALIVE'}, {})
+                expect_calls.ai_client.reset()
+            expect_calls.send_to_game('READY')
 
     def _patch_ai_client_reset(self) -> AIClient:
         # patch ai client
-        ai_client = MLPlay()
+        ai_client = self._patch_ai_client()
         ai_client.update = Mock(return_value="RESET")
-        ai_client.reset = Mock()
         return ai_client
 
-    @pytest.mark.parametrize(
-        'game_status_list',
-        [
-            [({'status': 'GAME_ALIVE'}, {})] * 5 + [({'status': 'GAME_OVER'}, {}), None],
-            [({'status': 'GAME_ALIVE'}, {})] * 10 + [({'status': 'GAME_OVER'}, {}), None]
-        ]
-    )
-    def test_ai_game_end(self, game_status_list):
-        ai_comm = self._create_mocked_ai_comm(game_status_list)
+    @pytest.mark.parametrize('game_alive_count', [5, 10])
+    def test_ai_game_end(self, game_alive_count):
+        game_status_list = [({'status': 'GAME_ALIVE'}, {})] * game_alive_count + [({'status': 'GAME_OVER'}, {}), None]
+        ai_comm, send_to_game = self._create_mocked_ai_comm(game_status_list)
         ai_client = self._patch_ai_client()
+
+        func_calls_test = Mock()
+        func_calls_test.ai_client = ai_client
+        func_calls_test.send_to_game = send_to_game.send
 
         executor = AIClientExecutor('tester', ai_comm, ai_loader=fixed_ai_loader(ai_client))
         executor.run()
 
-        ai_client.reset.assert_called_once()
-
-    @pytest.mark.parametrize(
-        'game_status_list',
-        [
-            [({'status': 'GAME_ALIVE'}, {})] * 5 + [({'status': 'GAME_OVER'}, {}), None],
-            [({'status': 'GAME_ALIVE'}, {})] * 10 + [({'status': 'GAME_OVER'}, {}), None]
-        ]
-    )
-    def test_ai_update(self, game_status_list):
-        ai_comm = self._create_mocked_ai_comm(game_status_list)
-        ai_client = self._patch_ai_client()
-
-        executor = AIClientExecutor('tester', ai_comm, ai_loader=fixed_ai_loader(ai_client))
-        executor.run()
-
-        assert ai_client.update.call_count == len(game_status_list) - 1
+        with assert_same(func_calls_test.mock_calls) as expect_calls:
+            expect_calls.send_to_game('READY')
+            for i in range(game_alive_count):
+                expect_calls.ai_client.update({'status': 'GAME_ALIVE'}, {})
+            expect_calls.ai_client.update({'status': 'GAME_OVER'}, {})
+            expect_calls.ai_client.reset()
+            expect_calls.send_to_game('READY')
 
     def _patch_ai_client(self) -> AIClient:
-        # patch ai client
-        ai_client = MLPlay()
-        ai_client.update = Mock()
-        ai_client.reset = Mock()
-        return ai_client
+        return Mock(MLPlay)
 
     def _create_mocked_ai_comm(self, game_status_list):
         ai_comm = MLCommManager('tester')
         recv_end = RecvEnd()
         recv_end.recv = Mock(side_effect=game_status_list)
-        ai_comm.set_comm_to_game(recv_end, SendEnd())
-        return ai_comm
+        send_end = SendEnd()
+        send_end.send = Mock()
+        ai_comm.set_comm_to_game(recv_end, send_end)
+        return ai_comm, send_end
+
